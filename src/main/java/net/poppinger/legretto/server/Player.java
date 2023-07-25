0a756ce0ce8103e0a2231075aee486fc49d4795f
Nora-it <Nora.Poppinger@it.htlhl.at>
2023-07-25 19:25:39 +0200

    changed stacktable full hight changed the colours to pastel

--- src/main/java/net/poppinger/legretto/server/Player.java
+++ src/main/java/net/poppinger/legretto/server/Player.java
@@ -9,42 +9,42 @@ public class Player {
     private int deckSlotPointer;
 
 
-    public Player(){
+    public Player() {
         resetSlots();
     }
 
-    public void resetSlots(){
-        slots=new int[4];
-        stackSlot=new int[10];
-        deckSlot=new int[40];
-        stackSlotPointer=-1;
-        deckSlotPointer=-1;
+    public void resetSlots() {
+        slots = new int[4];
+        stackSlot = new int[10];
+        deckSlot = new int[40];
+        stackSlotPointer = -1;
+        deckSlotPointer = -1;
     }
 
     /*
    returns boolean true....success
      */
-    public boolean addCardToStackslot(int card){
-        if (stackSlotPointer<stackSlot.length-1){
+    public boolean addCardToStackslot(int card) {
+        if (stackSlotPointer < stackSlot.length - 1) {
             stackSlotPointer++;
-            stackSlot[stackSlotPointer]=card;
+            stackSlot[stackSlotPointer] = card;
             return true;
         }
         return false;
     }
 
-    public int popCardFromStackslot(){
-        if (stackSlotPointer>0){
-            int card=stackSlot[stackSlotPointer];
+    public int popCardFromStackslot() {
+        if (stackSlotPointer > 0) {
+            int card = stackSlot[stackSlotPointer];
             stackSlotPointer--;
             return card;
         }
         return -1;
     }
 
-    public int getCardFromStackslot(){
-        if (stackSlotPointer>0){
-            int card=stackSlot[stackSlotPointer];
+    public int getCardFromStackslot() {
+        if (stackSlotPointer > 0) {
+            int card = stackSlot[stackSlotPointer];
             return card;
         }
         return -1;
@@ -54,27 +54,27 @@ public class Player {
     /*
 returns boolean true....success
  */
-    public boolean addCardToDeckSlot(int card){
-        if (deckSlotPointer<deckSlot.length-1){
+    public boolean addCardToDeckSlot(int card) {
+        if (deckSlotPointer < deckSlot.length - 1) {
             deckSlotPointer++;
-            deckSlot[deckSlotPointer]=card;
+            deckSlot[deckSlotPointer] = card;
             return true;
         }
         return false;
     }
 
-    public int popCardFromDeckSlot(){
-        if (deckSlotPointer>0){
-            int card=deckSlot[deckSlotPointer];
+    public int popCardFromDeckSlot() {
+        if (deckSlotPointer > 0) {
+            int card = deckSlot[deckSlotPointer];
             deckSlotPointer--;
             return card;
         }
         return -1;
     }
 
-    public int getCardFromDeckSlot(){
-        if (deckSlotPointer>0){
-            int card=deckSlot[deckSlotPointer];
+    public int getCardFromDeckSlot() {
+        if (deckSlotPointer > 0) {
+            int card = deckSlot[deckSlotPointer];
             return card;
         }
         return -1;
